dedd59cb8df3bcf31248d0683ee8b61de1c981a0
夜莺 <metastem@163.com>
2025-06-16 18:39:25 +0800

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="xuan",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[],
+    entry_points={
+        'console_scripts': [
+            'xuan=xuan.cli:main',
+        ],
+    },
+    author="Your Name",
+    author_email="your.email@example.com",
+    description="玄语言 - 一个中文编程语言",
+    long_description=open('README.md', encoding='utf-8').read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/yourusername/xuan",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
