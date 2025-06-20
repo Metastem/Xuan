b9072fe4fb1e3b057e701c0af8301bc1c7217f1d
夜莺 <metastem@163.com>
2025-06-20 12:20:30 +0800

    创建了 setup.py 文件以便 Python 能够正确识别 xuan 包

--- setup.py
+++ setup.py
@@ -5,21 +5,5 @@ setup(
     version="0.1.0",
     packages=find_packages(),
     install_requires=[],
-    entry_points={
-        'console_scripts': [
-            'xuan=xuan.cli:main',
-        ],
-    },
-    author="Metastem",
-    author_email="official@wl.ax",
-    description="玄语言 - 一个中文编程语言",
-    long_description=open('README.md', encoding='utf-8').read(),
-    long_description_content_type="text/markdown",
-    url="https://github.com/Metastem/xuan",
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: GNU License",
-        "Operating System :: OS Independent",
-    ],
-    python_requires='>=3.6',
+    python_requires=">=3.6",
 )
